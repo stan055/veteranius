97b9b75a92cd7171f846360023d4bbff6fa0d644
Stanislav Litvinyk <stanislav055@gmail.com>
2020-10-24 12:54:45 +0300

    week6api done again

--- week6api/app.js
+++ week6api/app.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-var PastebinAPI = require('pastebin-js')
+const express = require('express');
+const path = require('path');
+const request = require("request");
+const app = express();
+const axios = require('axios');
+var PastebinAPI = require('pastebin-js');
 
 process.env.PWD = process.cwd()
 app.use(express.static(process.env.PWD + '/client'));
@@ -19,8 +21,78 @@ app.get('/hard', (req, res) => {
 })
 
 app.use(express.json())
-const pasteKey = 'ie3KHhGNGAJb5olL38grQp9JM_FbeKJl'
+
+app.post('/api-aviationstack', (req, res) => {
+  url = 'http://api.aviationstack.com/v1/flights'
+  const aviaKey = 'cc3ff46cc95ac84190601c134ef11f6b'
+  let params = req.body
+  params.access_key = aviaKey
+  // let reqData = req.body
+  // reqData.access_key = aviaKey
+  // url = url+'?access_key='+reqData.access_key+'&flight_number='+reqData.flyght_number+'&flight_iata='+reqData.flight_iata+'&'
+  // console.log(reqData)
+  axios.get('http://api.aviationstack.com/v1/flights', {params})
+  .then(response => {
+    const apiResponse = response.data;
+    res.status(200).json(apiResponse)
+    // if (Array.isArray(apiResponse['results'])) {
+    //     apiResponse['results'].forEach(flight => {
+    //         if (!flight['live']['is_ground']) {
+    //             console.log(`${flight['airline']['name']} flight ${flight['flight']['iata']}`,
+    //                 `from ${flight['departure']['airport']} (${flight['departure']['iata']})`,
+    //                 `to ${flight['arrival']['airport']} (${flight['arrival']['iata']}) is in the air.`);
+    //         }
+    //     });
+    // }
+  }).catch(error => {
+    console.log(error);
+  });
+})
+
+app.post('/apiiss', (req, res) => {
+  const apiIss = 'http://api.open-notify.org/iss-now.json'
+
+  request.get(apiIss, (error, response, body) => {
+    if (JSON.parse(body).message == 'success')
+      res.status(200).json(JSON.parse(body))
+    else
+      res.status(501).json(JSON.parse(body))
+  });
+})
+
+app.post('/apiiss-pass', (req, res) => {
+  const lat = 50.4
+  const lon = 30.5
+  const alt = 100
+  const apiIss = `http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${lon}&alt=${alt}&n=1`
+
+  request.get(apiIss, (error, response, body) => {
+    var resolve = JSON.parse(body)
+    if(resolve.message == 'success') {
+      res.status(200).json(resolve)
+    }
+    else
+      res.status(501).json(body)
+  });
+})
+
+app.post('/api-serpstack', (req, res) => {
+  const key = '013dbcc35e77eb38d08a0b88c4e22cfd' 
+  const q = 'trident'
+  const url = 'http://api.serpstack.com/search?access_key='+key+'&type=web'+'&query='+q+'&page=2'+'&num=10'+'&gl=ua'+'&hl=uk'
+  request.get(url, (error, response, body) => {
+    if(body) {
+      var resolve = JSON.parse(body)
+      res.status(200).json(resolve)
+    } else {
+      res.status(501)
+    }
+    
+  })
+})
+
 app.post('/api', (req, res) => {
+  const pasteKey = 'ie3KHhGNGAJb5olL38grQp9JM_FbeKJl'
   pastebin = new PastebinAPI({
       'api_dev_key' : pasteKey,
      });
@@ -43,5 +115,5 @@ app.post('/api', (req, res) => {
 })
 
 
-
-app.listen(3000, () => console.log('Server has been started on port 3000...'))
+const PORT = process.env.PORT || 80
+app.listen(PORT, () => console.log('Server has been started on port 3000...'))
